84d2222ed140e4f843e7e965fbf652d7b04f08f5
Anina Klaus <76693508+a-klaus@users.noreply.github.com>
2025-03-16 22:16:42 +0100

    Refactor learning curve plotting

--- create_dataset.py
+++ create_dataset.py
@@ -20,52 +20,50 @@ from functools import partialmethod
 tqdm.__init__ = partialmethod(tqdm.__init__, disable=True)
 
 
-if __name__ == "__main__":
+parser = argparse.ArgumentParser()
+parser.add_argument("data_dir", type=str, help="Path to the directory containing the data files, each consisting lines of raw sentences")
+parser.add_argument("out_dir", type=str, help="Directory to store the output_files at, will be created")
+parser.add_argument("--seq-len", required=True, type=int, help="Length to which the sequences are to be packed")
+parser.add_argument("--file-len", required=False, type=int, default=100000, help="How many lines to store in a file, defaults to 100000")
+parser.add_argument("--tokenizer", required=False, type=str, default="bert-base-cased", help="Identifier of the tokenizer to use, defaults to 'bert-base-cased'")
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("data_dir", type=str, help="Path to the directory containing the data files, each consisting lines of raw sentences")
-    parser.add_argument("out_dir", type=str, help="Directory to store the output_files at, will be created")
-    parser.add_argument("--seq-len", required=True, type=int, help="Length to which the sequences are to be packed")
-    parser.add_argument("--file-len", required=False, type=int, default=100000, help="How many lines to store in a file, defaults to 100000")
-    parser.add_argument("--tokenizer", required=False, type=str, default="bert-base-cased", help="Identifier of the tokenizer to use, defaults to 'bert-base-cased'")
+args = parser.parse_args()
 
-    args = parser.parse_args()
+# Make sure all provided arguments are valid
+assert os.path.isdir(args.data_dir), "Data directory not found"
+os.mkdir(args.out_dir)  # Try to create directory for output (may not exist beforehand)
 
-    # Make sure all provided arguments are valid
-    assert os.path.isdir(args.data_dir), "Data directory not found"
-    os.mkdir(args.out_dir)  # Try to create directory for output (may not exist beforehand)
+# Set up tokenizer
+tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
 
-    # Set up tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
+# Create dataset
+gurks = PackedCucumbers(args.data_dir, tokenizer, max_seq_len=args.seq_len)
+ds = gurks.create_giant_dataset()
+out_dir = args.out_dir
 
-    # Create dataset
-    gurks = PackedCucumbers(args.data_dir, tokenizer, max_seq_len=args.seq_len)
-    ds = gurks.create_giant_dataset()
-    out_dir = args.out_dir
+# Save
+file_len = args.file_len
+c = 0  # Counter
+lines = []
+for instance in ds:
+    # If file_len limit is reached: save to file
+    if len(lines) == file_len:
+        # Create file name
+        file_name = f"file{c-len(lines)}_{c}.jsonlines"
+        with open(os.path.join(out_dir, file_name), "w", encoding="utf-8") as f:
+            for line in lines:
+                json.dump(line, f)
+                f.write("\n")
+        lines = []  # Reset lines
+    # Collect data items for current file
+    instance["input_ids"] = instance["input_ids"].tolist()
+    lines.append(instance)
+    c += 1
 
-    # Save
-    file_len = args.file_len
-    c = 0  # Counter
-    lines = []
-    for instance in ds:
-        # If file_len limit is reached: save to file
-        if len(lines) == file_len:
-            # Create file name
-            file_name = f"file{c-len(lines)}_{c}.jsonlines"
-            with open(os.path.join(out_dir, file_name), "w", encoding="utf-8") as f:
-                for line in lines:
-                    json.dump(line, f)
-                    f.write("\n")
-            lines = []  # Reset lines
-        # Collect data items for current file
-        instance["input_ids"] = instance["input_ids"].tolist()
-        lines.append(instance)
-        c += 1
-
-    # Save final lines as well
-    file_name = f"file{c-len(lines)}_{c}.jsonlines"
-    with open(os.path.join(out_dir, file_name), "w", encoding="utf-8") as f:
-        for line in lines:
-            json.dump(line, f)
-            f.write("\n")
+# Save final lines as well
+file_name = f"file{c-len(lines)}_{c}.jsonlines"
+with open(os.path.join(out_dir, file_name), "w", encoding="utf-8") as f:
+    for line in lines:
+        json.dump(line, f)
+        f.write("\n")
 
--- plot_learning_curves.py
+++ plot_learning_curves.py
@@ -1,47 +1,85 @@
+import argparse
 import re
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-logfile = "logs/dec22.log"
+# Set up parser
+parser = argparse.ArgumentParser()
+parser.add_argument("log_file", type=str, help="Path to the log file containing the log output of the model")
+parser.add_argument("config_file", type=str, help="Path to the config file used for the training run")
+parser.add_argument("out_dir", type=str, help="Directory to store the output files at")
+parser.add_argument("--plot-steps", type=int, default=200, help="Number of steps to average over for plotting the training data learning curve")
 
-# Batch loss on step 112799: 5.48	Batch accuracy@3: 0.20	ms/batch: 1042.53
-# Validation loss: 5.96	Validation accuracy@3 0.15   
-# pattern = r"^Batch loss on step \d+: ([\d\.]+).*Batch accuracy@3: ([\d\.]+)	ms/batch: .*$"
-pattern = r"^Validation loss: (\d.\d\d).*Validation accuracy\@3 (\d.\d\d).*$"
+args = parser.parse_args()
 
-# x = 1
-loss, acc = [], []
-loss_avg, acc_avg = [], []
+logfile = args.log_file
+out_dir = args.out_dir
+config_name = os.path.split(args.config_file)[1].split(".")[0]
+
+pattern_training = r"^Batch loss on step \d+: ([\d\.]+).*Batch accuracy@3: ([\d\.]+)	ms/batch: .*$"
+pattern_validation = r"^Validation loss: (\d.\d\d).*Validation accuracy\@3 (\d.\d\d).*$"
+
+# Set up lists to collect values
+val_loss, val_acc = [], []
+train_loss, train_acc = [], []
+train_loss_avg, train_acc_avg = [], []
+# Collect values from log file
 with open(logfile, encoding="utf-8") as file_in:
     for line in file_in:
-        # if line.startswith("Batch"):
+        # Validation results line
         if line.startswith("Validation"):
-            print(line)
-            mo = re.match(pattern, line)
-            loss.append(float(mo.group(1)))
-            acc.append(float(mo.group(2)))
-        # if line.startswith("Step"):
-        # if len(loss) == 5000:
-            # loss = np.array(loss) 
-            # acc = np.array(acc) 
-            # loss_avg.append(loss.mean())
-            # acc_avg.append(acc.mean())
-            # loss, acc = [], []
+            mo = re.match(pattern_validation, line)
+            val_loss.append(float(mo.group(1)))
+            val_acc.append(float(mo.group(2)))
+        # Training batch result
+        if line.startswith("Batch"):
+            mo = re.match(pattern_training, line)
+            train_loss.append(float(mo.group(1)))
+            train_acc.append(float(mo.group(2)))
+        # Average over steps
+        if len(train_loss) == args.plot_steps:
+            train_loss = np.array(train_loss) 
+            train_acc = np.array(train_acc) 
+            train_loss_avg.append(train_loss.mean())
+            train_acc_avg.append(train_acc.mean())
+            train_loss, train_acc = [], []
         
+# Include final steps as well
+train_loss = np.array(train_loss) 
+train_acc = np.array(train_acc) 
+train_loss_avg.append(train_loss.mean())
+train_acc_avg.append(train_acc.mean())
 
-loss = np.array(loss) 
-acc = np.array(acc) 
-loss_avg.append(loss.mean())
-acc_avg.append(acc.mean())
+# Plot training accuracy
+plt.figure()
+plt.plot(train_acc_avg)
+plt.title(f"Train accuracy each {args.plot_steps} steps")
+filename = f"{config_name}_training_accuracy_curve.png"
+plt.savefig(os.path.join(out_dir, filename))
 
-# print(loss_avg)
-# print(acc_avg)
-print(loss)
+# Plot training loss
+plt.figure()
+plt.plot(train_loss_avg)
+plt.title(f"Train loss averaged each {args.plot_steps} steps")
+filename = f"{config_name}_training_loss_curve.png"
+plt.savefig(os.path.join(out_dir, filename))
 
-# plt.plot(acc_avg)
-plt.plot(acc)
-plt.title("Validation accuracy")
-# plt.show()
-plt.savefig("logs/dec22_val_acc_t.png")
+# Plot validation accuracy
+plt.figure()
+plt.plot(val_acc)
+plt.title(f"Validation accuracy each {args.plot_steps} steps")
+filename = f"{config_name}_validation_accuracy_curve.png"
+plt.savefig(os.path.join(out_dir, filename))
+
+# Plot training loss
+plt.figure()
+plt.plot(val_loss)
+plt.title(f"Validation loss averaged each {args.plot_steps} steps")
+filename = f"{config_name}_validation_loss_curve.png"
+plt.savefig(os.path.join(out_dir, filename))
+
+print(f"Plots saved under {out_dir}")
 
